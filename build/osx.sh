1270fde4cb5038881c6698a7d536cbe580725ada
Amos Wenger <amos@lockfree.ch>
2013-03-29 00:11:49 +0100

    Correct build options on OSX for 10.7 compat

--- build/osx.sh
+++ build/osx.sh
@@ -1,2 +1,5 @@
-export MACOSX_DEPLOYMENT_TARGET=10.6
-rock -v -g -j3 +-headerpad_max_install_names +-mmacosx-version-min=10.6
+#!/bin/bash
+export MACOSX_DEPLOYMENT_TARGET="10.7"
+export CFLAGS="-mmacosx-version-min=10.7 -isysroot /Users/amos/Dev/MacOSX10.7.sdk"
+export LDFLAGS="-Wl,-syslibroot,/Users/amos/Dev/MacOSX10.7.sdk"
+rock -v -g -j3 +-headerpad_max_install_names
